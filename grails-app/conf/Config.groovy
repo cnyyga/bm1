347165006d9d35354f22a35aa2d8fcef2cc2e781
cnyyga <cnyyga@gmail.com>
2013-12-20 01:42:07 -0800

    log

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -100,9 +100,14 @@ log4j = {
         environments {
             production {
                 new org.apache.log4j.DailyRollingFileAppender(name: "dailyAppender",
-                        layout: pattern(conversionPattern: '%d{HH:mm:ss} %l %n[%5p] %m%n'), fileName: "/d:/log/bm.log", datePattern: "yyyy-MM-dd")
+                        layout: pattern(conversionPattern: '%d{HH:mm:ss} %l %n[%5p] %m%n'), fileName: "d:/log/bm.log", datePattern: "yyyy-MM-dd")
             }
         }
+    }
+
+    root {
+        error 'dailyAppender', 'stdout'
+        additivity = true
     }
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
